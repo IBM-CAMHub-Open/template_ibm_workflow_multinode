4871a74d16ddf2ecde8271a2278837f45db4531c
CAMHub PublishBot <octravis@us.ibm.com>
2019-04-03 14:30:36 +0000

    Automated CAMHub::publish on 2019-04-03 14:30:36 UTC

--- vmware/terraform/ibm_workflow_v18_multinode.tf
+++ vmware/terraform/ibm_workflow_v18_multinode.tf
@@ -1903,7 +1903,7 @@ resource "camc_softwaredeploy" "Workflow01_workflow_v18_upgrade" {
           }
       },
       "install_dir": "${local.Workflow01_install_dir}",
-      "fixpack_names": ["${join("\",\"", var.Workflow01_fixpack_names)}"],
+      "fixpack_names_list": "${join(",", var.Workflow01_fixpack_names)}",
       "config": {
        "node_hostnames": "${var.Workflow01-name}.${var.Workflow01_domain},${var.Workflow02-name}.${var.Workflow02_domain}",
        "celladmin_alias_user": "${var.Workflow01_cell_admin_username}"
@@ -2706,7 +2706,7 @@ resource "camc_softwaredeploy" "Workflow02_workflow_v18_upgrade" {
           }
       },
       "install_dir": "${local.Workflow02_install_dir}",
-      "fixpack_names": ["${join("\",\"", var.Workflow01_fixpack_names)}"],
+      "fixpack_names_list": "${join(",", var.Workflow01_fixpack_names)}",
       "config": {
        "node_hostnames": "${var.Workflow01-name}.${var.Workflow01_domain},${var.Workflow02-name}.${var.Workflow02_domain}",
        "celladmin_alias_user": "${var.Workflow01_cell_admin_username}"
